7e8b016562d0a2a271dc871bb473961d9994347a
ethan9452 <ethan9452@gmail.com>
2019-07-10 00:45:35 -0700

    Revert "asdf"
    This reverts commit e7a9a1c71cfd1d5c46555c1b96e1c5f417566cd6.

--- src/collisiondetectprovidor/ICollisionDetectionProvidor.java
+++ src/collisiondetectprovidor/ICollisionDetectionProvidor.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class ICollisionDetectionProvidor
+public interface ICollisionDetectionProvidor
 {
 	public static List<Class<? extends ICollisionDetectionProvidor>> IMPLEMENTED_PROVIDORS = new ArrayList<Class<? extends ICollisionDetectionProvidor>>()
 	{
@@ -13,6 +13,19 @@ public abstract class ICollisionDetectionProvidor
 		}
 	};
 
-	public boolean isColliding( Point p, List<Point> points);
+	/**
+	 * Should find all colliding paris in `points` and call `detectionCallback`
+	 * on each pair
+	 *
+	 * Note: implementing methods must call `detectionCallback` EXACTLY ONCE on
+	 * each collided pair
+	 *
+	 * Note: 2 same points shouldn't count as a collision
+	 * 
+	 * @param points
+	 * @param detectionCallback
+	 */
+	public void runCollisionDetect( List<Point> points, ICollisionCallback detectionCallback );
+	
 	
 }
